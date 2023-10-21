412e1b4bb5bfbfdc2bb913490243a84371ca5fc2
Anna Dai <anna.dai916@gmail.com>
2023-10-21 16:15:22 -0400

    Add global variable for money spent

--- app/money_calculator.py
+++ app/money_calculator.py
@@ -4,6 +4,15 @@ boba - $5.84
 Bottled water 16.9 oz - $1.29, so ~0.07633/fl. oz, so ~$0.611 per cup
 Cooking at home - $7.00
 '''
+money_spent = 0
+
+def add_money_spent(cost):
+    global money_spent
+    money_spent -= cost
+
+def add_completed_task(task):
+    pass
+
 def gas_money(miles):
     return miles * 0.655
 
