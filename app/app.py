d1bbac12887e3d3cb8ce22c4fac56999d58836e8
Anna Dai <anna.dai916@gmail.com>
2023-10-21 19:03:22 -0400

    Set up socketio

--- app/app.py
+++ app/app.py
@@ -1,10 +1,20 @@
 from flask import Flask, render_template
+from flask_socketio import SocketIO
+from money_calculator import *
 
 app = Flask(__name__)
+socketio = SocketIO(app)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@socketio.on('calculate')
+def handle_calculation(data):
+    global money_spent
+    cost_difference = data['costDifference']
+    money_spent -= cost_difference
+    socketio.emit('result', {'result': money_spent})
+
 if __name__ == '__main__':
-    app.run(debug=True, port='4000')
+    socketio.run(app, debug=True)
